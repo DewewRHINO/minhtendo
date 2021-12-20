cde18ee2c6864952034f61a3e96c0f5b7d78212c
Takao21 <42912031+Takao21@users.noreply.github.com>
2021-12-21 02:31:39 +0630

    desktop nav layout OK

--- src/common_components/NavBar.js
+++ src/common_components/NavBar.js
@@ -5,45 +5,58 @@ import { Link } from "react-router-dom";
 export const NavBar = () => {
   return (
     <div className="NavBar">
-      <div className="nav-section nav-sec-1 container-fluid">
-        <div className="row nav-sec-1-row">
-          <div className="col-2 logo-col">
-            <img
-              src={require("../media/landscape-logo.png")}
-              alt="landscape logo"
-            />
-          </div>
-          <div className="col-3 search-col">
+      <div className="nav-section nav-sec-1">
+        <div className="brand-box">
+          <img src={require("../media/landscape-logo.png")} alt="brand" />
+        </div>
+        <div className="search-box">
+          <div className="search-box-content">
             <label htmlFor="searchfield" className="searchlabel">
               &#128269;
             </label>
             <input
               className="searchfield"
               placeholder="Search games, systems, supports, etc."
+              size={50}
             />
           </div>
-          <div className="col-4 spacing-col"> </div>
-          <div className="col-1 btn-col">Support</div>
-          <div className="col-1 btn-col">Wish List</div>
-          <div className="col-1 btn-col">Login/Signup</div>
+        </div>
+        <div className="nav-buttons">
+          <div className="nav-btn-box">
+            <Link to="/" className="my-nav-btn">
+              <span>⚫ Support</span>
+            </Link>
+          </div>
+          <div className="nav-btn-box">
+            <Link to="/" className="my-nav-btn">
+              <span>⚫ Wish List</span>
+            </Link>
+          </div>
+          <div className="nav-btn-box">
+            <Link to="/" className="my-nav-btn">
+              <span>⚫ Login / Signup</span>
+            </Link>
+          </div>
         </div>
       </div>
       <div className="nav-section nav-sec-2">
-        <Link to="/">
-          <button>Home</button>
-        </Link>
-        <Link to="/games">
-          <button>Games</button>
-        </Link>
-        <Link to="/hardwares">
-          <button>Hardware</button>
-        </Link>
-        <Link to="/whatsnew">
-          <button>News</button>
-        </Link>
-        <Link to="/holiday">
-          <button>Holiday Gifts</button>
-        </Link>
+        <div className="nav-sec-2-content">
+          <Link to="/games" className="my-nav-btn">
+            <span>Games</span>
+          </Link>
+          <Link to="/hardwares" className="my-nav-btn">
+            <span>Hardware</span>
+          </Link>
+          <Link to="/whatsnew" className="my-nav-btn">
+            <span>News & Events</span>
+          </Link>
+          <Link to="/playnintendo" className="my-nav-btn">
+            <span>Play Nintendo</span>
+          </Link>
+          <Link to="/holiday" className="my-nav-btn">
+            <span>Holiday Gifts Guide</span>
+          </Link>
+        </div>
       </div>
     </div>
   );
