fcc7f7c406073afa58809b4e31fd982e8fbac5c8
Takao21 <42912031+Takao21@users.noreply.github.com>
2021-12-20 19:56:02 +0630

    logo and search bar in progress

--- src/common_components/NavBar.js
+++ src/common_components/NavBar.js
@@ -5,7 +5,29 @@ import { Link } from "react-router-dom";
 export const NavBar = () => {
   return (
     <div className="NavBar">
-      <div className="nav-section nav-sec-1">Main Section Of Nav</div>
+      <div className="nav-section nav-sec-1 container-fluid">
+        <div className="row nav-sec-1-row">
+          <div className="col-2 logo-col">
+            <img
+              src={require("../media/landscape-logo.png")}
+              alt="landscape logo"
+            />
+          </div>
+          <div className="col-3 search-col">
+            <label htmlFor="searchfield" className="searchlabel">
+              &#128269;
+            </label>
+            <input
+              className="searchfield"
+              placeholder="Search games, systems, supports, etc."
+            />
+          </div>
+          <div className="col-4 spacing-col"> </div>
+          <div className="col-1 btn-col">Support</div>
+          <div className="col-1 btn-col">Wish List</div>
+          <div className="col-1 btn-col">Login/Signup</div>
+        </div>
+      </div>
       <div className="nav-section nav-sec-2">
         <Link to="/">
           <button>Home</button>
