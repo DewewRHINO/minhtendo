b9a9b4793030b9f7e5828ce7910619ed51987cff
Takao21 <42912031+Takao21@users.noreply.github.com>
2021-12-20 18:40:04 +0630

    router setup OK

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import { Footer } from "./common_components/Footer";
 import { NavBar } from "./common_components/NavBar";
 import { WebRoadmap } from "./common_components/WebRoadmap";
 import { PageContent } from "./components/PageContent";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <PageContent />
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<PageContent />} />
+          <Route path="/testing" element={<>This is a testing page.</>} />
+        </Routes>
+      </Router>
+
       <WebRoadmap />
       <Footer />
     </div>
