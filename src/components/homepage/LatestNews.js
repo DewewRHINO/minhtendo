3668e26ac4a40506aeddf38808527b771fe24eac
Takao21 <42912031+Takao21@users.noreply.github.com>
2022-01-04 02:57:22 +0630

    created util func to convert date

--- src/components/homepage/LatestNews.js
+++ src/components/homepage/LatestNews.js
@@ -1,22 +1,37 @@
 import React from "react";
 import "./LatestNews.css";
+import { toMMDDYY } from "../../util/conversions";
 
 export const LatestNews = ({ data }) => {
   const allNews = [...data];
   const newToOldSortedNews = allNews.sort(
     (a, b) => Number(b.postDateEpoch) - Number(a.postDateEpoch)
   );
-  const mostRecentNews = newToOldSortedNews.slice(0, 2);
-  const olderNews = newToOldSortedNews.slice(-4);
-  console.log(newToOldSortedNews, mostRecentNews, olderNews);
+  const formattedNews = [...newToOldSortedNews];
+  for (let i = 0; i < formattedNews.length; i++) {
+    formattedNews[i].date = toMMDDYY(formattedNews[i].postDateEpoch);
+  }
+  const mostRecentNews = formattedNews.slice(0, 2);
+  const olderNews = formattedNews.slice(-4);
   return (
     <div className="LatestNews">
-      {mostRecentNews.map((news) => (
-        <div key={news.id}>recent news</div>
-      ))}
-      {olderNews.map((news) => (
-        <div key={news.id}>older news</div>
-      ))}
+      <div className="big-size-news">
+        {mostRecentNews.map((news) => (
+          <div className="news-card" key={news.id}>
+            <div className="card-img-container">
+              <img src={news.postImg} alt={news.postImgAlt} />
+              <p>{news.date}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="small-size-news">
+        {olderNews.map((news) => (
+          <div className="news-card" key={news.id}>
+            older news
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/util/conversions.js
@@ -0,0 +1,8 @@
+export const toMMDDYY = (epochSeconds) => {
+  const date = new Date(epochSeconds * 1000);
+  const formattedDate = `${date.getMonth()}/${date.getDate()}/${date
+    .getFullYear()
+    .toString()
+    .slice(-2)}`;
+  return formattedDate;
+};
