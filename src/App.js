a7c7c09da0696ce4eb5dfe11f3d6f3799951797f
Takao21 <42912031+Takao21@users.noreply.github.com>
2022-01-07 01:36:19 +0630

    toggle nav on scroll

--- src/App.js
+++ src/App.js
@@ -8,12 +8,43 @@ import { GamesPage } from "./pages/GamesPage";
 import { HardwarePage } from "./pages/HardwarePage";
 import { NewsPage } from "./pages/NewsPage";
 import { HolidayPage } from "./pages/HolidayPage";
+import { useEffect, useState } from "react";
 
 function App() {
+  let prevScroll = window.scrollY;
+  const [goingUp, setGoingUp] = useState(false);
+
+  useEffect(() => {
+    document.addEventListener("scroll", handleNavToggle);
+    return () => {
+      document.removeEventListener("scroll", handleNavToggle);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    console.log("Re-rendering the app.");
+  });
+
+  const handleNavToggle = () => {
+    console.log(
+      "Last scroll was: ",
+      prevScroll,
+      "Current scroll is: ",
+      window.scrollY
+    );
+    if (prevScroll > window.scrollY) {
+      setGoingUp(true);
+    } else if (prevScroll < window.scrollY) {
+      setGoingUp(false);
+    }
+    prevScroll = window.scrollY;
+  };
+
   return (
     <Router>
       <div className="App">
-        <NavBar />
+        {goingUp && <NavBar />}
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/games" element={<GamesPage />} />
