953e7d715b0ec929ae963f7df8c3ddcdbe62fad1
Takao21 <42912031+Takao21@users.noreply.github.com>
2022-01-09 00:58:27 +0630

    open/close search OK

--- src/common_components/NavBar.js
+++ src/common_components/NavBar.js
@@ -4,6 +4,13 @@ import { Link } from "react-router-dom";
 import BrandLogo from "../media/landscape-logo.png";
 
 export const NavBar = ({ innerRef, setOpenSearch, openSearch }) => {
+  const handleCloseSearch = () => {
+    console.log("handleCloseSearch");
+    setTimeout(() => {
+      console.log("Waited");
+      return { visibility: "hidden" };
+    }, 3000);
+  };
   return (
     <div className="NavBar toggle-nav-in" ref={innerRef}>
       <div className="nav-section nav-sec-1">
@@ -15,7 +22,6 @@ export const NavBar = ({ innerRef, setOpenSearch, openSearch }) => {
         <div className="search-box">
           <div
             className="search-box-content"
-            // onClick={() => setOpenSearch(true)}
             onClick={() => setOpenSearch(true)}
           >
             <label htmlFor="searchfield" className="searchlabel">
@@ -28,74 +34,88 @@ export const NavBar = ({ innerRef, setOpenSearch, openSearch }) => {
               tabIndex={-1}
             />
           </div>
-          {openSearch && (
-            <div className="search-assist">
+          <div
+            className="search-assist"
+            style={
+              openSearch ? { pointerEvents: "auto" } : { pointerEvents: "none" }
+            }
+          >
+            <div
+              className="overlay-shadow"
+              onClick={() => setOpenSearch(false)}
+              style={openSearch ? { opacity: "0.8" } : { opacity: "0" }}
+            ></div>
+            <div
+              className="search-assist-content"
+              style={
+                openSearch === true
+                  ? { transform: "scaleY(1)" }
+                  : { transform: "scaleY(0)" }
+              }
+            >
+              <div className="nav-section nav-sec-1 duplicate-logo-and-input">
+                <div className="brand-box">
+                  <Link to="">
+                    <img src={BrandLogo} alt="brand" />
+                  </Link>
+                </div>
+                <div className="search-box">
+                  <div className="search-box-content">
+                    <label htmlFor="searchfield" className="searchlabel">
+                      &#128269;
+                    </label>
+                    <input
+                      className="searchfield"
+                      placeholder="Search Nintendo.com"
+                      size={50}
+                      autoFocus
+                    />
+                  </div>
+                  <span
+                    className="close-search-btn"
+                    onClick={() => setOpenSearch(false)}
+                  >
+                    X
+                  </span>
+                </div>
+              </div>
               <div
-                className="overlay-shadow"
-                onClick={() => setOpenSearch(false)}
-              ></div>
-              <div className="search-assist-content">
-                <div className="nav-section nav-sec-1 duplicate-logo-and-input">
-                  <div className="brand-box">
-                    <Link to="">
-                      <img src={BrandLogo} alt="brand" />
+                className="search-suggestion-box"
+                style={openSearch ? { opacity: "1" } : { opacity: "0" }}
+              >
+                <div className="quick-links">
+                  <div className="suggestions">
+                    <p className="suggestion-title">Suggestions</p>
+                    <Link to="" className="suggestion-link">
+                      <p>Nintendo Switch</p>
+                    </Link>
+                    <Link to="" className="suggestion-link">
+                      <p>Nintendo Switch Games</p>
+                    </Link>
+                    <Link to="" className="suggestion-link">
+                      <p>Nintendo Switch Online</p>
+                    </Link>
+                    <Link to="" className="suggestion-link">
+                      <p>Nintendo news</p>
+                    </Link>
+                    <Link to="" className="suggestion-link">
+                      <p>Ring Fit Adventure</p>
+                    </Link>
+                    <Link to="" className="suggestion-link">
+                      <p>Mario games</p>
+                    </Link>
+                    <Link to="" className="suggestion-link">
+                      <p>Zelda games</p>
+                    </Link>
+                    <Link to="" className="suggestion-link">
+                      <p>Pokemon games</p>
                     </Link>
                   </div>
-                  <div className="search-box">
-                    <div className="search-box-content">
-                      <label htmlFor="searchfield" className="searchlabel">
-                        &#128269;
-                      </label>
-                      <input
-                        className="searchfield"
-                        placeholder="Search Nintendo.com"
-                        size={50}
-                        autoFocus
-                      />
-                    </div>
-                    <span
-                      className="close-search-btn"
-                      onClick={() => setOpenSearch(false)}
-                    >
-                      X
-                    </span>
-                  </div>
-                </div>
-                <div className="search-suggestion-box">
-                  <div className="quick-links">
-                    <div className="suggestions">
-                      <p className="suggestion-title">Suggestions</p>
-                      <Link to="" className="suggestion-link">
-                        <p>Nintendo Switch</p>
-                      </Link>
-                      <Link to="" className="suggestion-link">
-                        <p>Nintendo Switch Games</p>
-                      </Link>
-                      <Link to="" className="suggestion-link">
-                        <p>Nintendo Switch Online</p>
-                      </Link>
-                      <Link to="" className="suggestion-link">
-                        <p>Nintendo news</p>
-                      </Link>
-                      <Link to="" className="suggestion-link">
-                        <p>Ring Fit Adventure</p>
-                      </Link>
-                      <Link to="" className="suggestion-link">
-                        <p>Mario games</p>
-                      </Link>
-                      <Link to="" className="suggestion-link">
-                        <p>Zelda games</p>
-                      </Link>
-                      <Link to="" className="suggestion-link">
-                        <p>Pokemon games</p>
-                      </Link>
-                    </div>
-                    <div className="link-cards"></div>
-                  </div>
+                  <div className="link-cards"></div>
                 </div>
               </div>
             </div>
-          )}
+          </div>
         </div>
         <div className="nav-buttons">
           <div className="nav-btn-box">
