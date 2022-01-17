4008fa64d1cc25b2eb51230f85f16843ea32a2c1
Takao21 <42912031+Takao21@users.noreply.github.com>
2022-01-17 22:00:30 +0630

    code cleanup

--- src/common_components/WebRoadmap.js
+++ src/common_components/WebRoadmap.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./WebRoadmap.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
